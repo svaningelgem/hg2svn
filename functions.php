788f31c4786c8e4175d4462417c493c502f721c5
s.vaningelgem@gmail.com <s.vaningelgem@gmail.com@924969ee-89ad-3269-b9ab-9054e7bfb4fe>
2011-08-09 12:46:43 +0000

    Added todo entries & reworked structure a bit

--- functions.php
+++ functions.php
@@ -1,36 +1,135 @@
 <?php
-function usage($msg) {
-    echo $msg;
-    exit(1);
-}
+  ##########################################
+  # Cmdline parsing
+  $verbose = 0;
+  foreach( $_SERVER['argv'] as $idx => $parameter ) {
+    $reworked = true;
 
-function get_clean_name($mercurial_src) {
-    //Extract "clean" repo name for our folders.
-    if (is_dir($mercurial_src)) {
-        //clean name is base name of folder.
-        $repo_name = basename(rtrim($mercurial_src, '/'));
-        return $repo_name;
-    } else {
-        //Assume an url, extract last word.
-        $repo_name = rtrim(shell_exec("echo $mercurial_src | awk -F \/ {'print \$NF'}"));
-        return $repo_name;
-    }
-}
+    switch( strtolower($parameter) ) {
+    case '-v':
+    case '--verbose':
+      $verbose++;
+      break;
 
-function create_svn_repo($svn_repo,$subversion_target) {
-    if (file_exists($svn_repo) || file_exists($subversion_target)) {
-        echo "Error: $subversion_repo or $subversion_target must NOT exist when init is specified.  Aborting.\n";
-        exit(1);
-    } else {
-        echo_verbose("Creating new svn repo, and checking it out as $subversion_target\n");
-        shell_exec("svnadmin create $svn_repo");
-        shell_exec("svn co file://$svn_repo $subversion_target");
+    case '-h':
+    case '--help':
+      usage();
+      break;
+
+    default:
+      $reworked = false;
+      break;
     }
-}
+
+    if ( $reworked ) {
+      unset($_SERVER['argv'][$idx]);
+    }
+  }
+  define('VERBOSE_OUTPUT', $verbose);
+  define('VERBOSE_LOW'   , 0);
+  define('VERBOSE_NORMAL', 1);
+  define('VERBOSE_HIGH'  , 2);
+  define('VERBOSE_DEBUG' , 3);
   
-function echo_verbose($message) {
-    if (VERBOSITY) {
-        echo $message;
+  unset($verbose);
+
+
+  $_SERVER['argv'] = array_values($_SERVER['argv']); # reset indexes of this array
+  $_SERVER['argc'] = count($_SERVER['argv']);
+  ##########################################
+
+
+  ##########################################
+  # Config file loading
+  $config = parse_ini_file(dirname(__FILE__) . "/config.ini");
+  ##########################################
+
+
+
+
+
+
+
+  ##########################################
+  # Functions
+  ##########################################
+
+  function usage($msg = '') {
+    if ( $msg != '' ) {
+      echo "!!! ".trim($msg)."\n\n";
     }
-}
-?>
+
+    echo "Usage: {$_SERVER['argv'][0]} [--verbose|-v] [--help|-h] init|sync\n";
+    echo " with:    init <hg repository> <svn repository>\n";
+    echo " with:    sync <svn repository>\n";
+
+    exit(1);
+  }
+
+  function get_clean_name($mercurial_src) {
+      //Extract "clean" repo name for our folders.
+      if (is_dir($mercurial_src)) {
+          //clean name is base name of folder.
+          $repo_name = basename(rtrim($mercurial_src, '/'));
+          return $repo_name;
+      } else {
+          //Assume an url, extract last word.
+          $repo_name = rtrim(shell_exec("echo $mercurial_src | awk -F \/ {'print \$NF'}"));
+          return $repo_name;
+      }
+  }
+
+  /**
+  * Creates the directory structures needed for hg/svn work
+  * 
+  * @returns string temporary directory name to be used
+  */
+  function create_directory_structure() {
+    $tempnam = tempnam(get_cache_dir(), 'hg2svn');
+
+    @mkdir($tempnam.'_hg', 0755, true);
+    @mkdir($tempnam.'_svn', 0755, true);
+
+    return $tempnam;
+  }
+
+  function create_svn_repo($svn_repo,$subversion_target) {
+      if (file_exists($svn_repo) || file_exists($subversion_target)) {
+          echo "Error: $subversion_repo or $subversion_target must NOT exist when init is specified.  Aborting.\n";
+          exit(1);
+      } else {
+          echo_verbose("Creating new svn repo, and checking it out as $subversion_target\n");
+          shell_exec("svnadmin create $svn_repo");
+          shell_exec("svn co file://$svn_repo $subversion_target");
+      }
+  }
+    
+  function cout($message, $level = 0) {
+    if ($level < VERBOSE_OUTPUT) {
+      return;
+    }
+
+    echo $message . "\n";
+  }
+
+  function get_cache_dir( ) {
+    static $cache_dir = null;
+    if ( is_null($cache_dir) ) {
+      global $config;
+
+      $cache_dir = $config['cache_dir'];
+
+      is_dir($cache_dir) || (
+        @mkdir($cache_dir, 0755, true)
+        &&
+        cout("Succesfully created {$cache_dir}", VERBOSE_NORMAL)
+      );
+
+      if ( !is_writable($cache_dir) ) {
+        cout("Error: Could not create {$cache_dir} please check permissions.");
+        exit(1);
+      }
+    }
+
+    return $cache_dir;
+  }
