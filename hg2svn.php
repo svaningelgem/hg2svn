26d9af385d75e38fb5ffdf6d8123f7bd2105cb49
pieterste@gmail.com <pieterste@gmail.com@924969ee-89ad-3269-b9ab-9054e7bfb4fe>
2011-08-09 22:31:01 +0000

    Fix init()

--- hg2svn.php
+++ hg2svn.php
@@ -29,15 +29,13 @@
   }
   exit(0);
 
-
-
   // **TODO**: What if I give 2 remote urls?
   // **TODO**:  --> store data in revision 0 of the subversion repository of where we left off etc.
 
   function init() {
     // **TODO** @steven@ Check how we can see if a revision property can be modified (it's a hook somewhere I know) & fail if this won't work.
-    $from_hg = $_SERVER['argv'][2];
-    $to_svn  = $_SERVER['argv'][3];
+    $from_hg_repo = $_SERVER['argv'][2];
+    $from_svn_repo  = $_SERVER['argv'][3];
 
     # @ pieter: Contact "to_svn" & get properties of SVN level 0 to see if we need to use a special cache directory
     #  --> Adjust create_directory_structure() to take in an optional name which checks if it already exists & creates if not, if it already exists exit with error.
@@ -47,7 +45,7 @@
     $svn_target = $checkout_directory.'_svn'; 
     
     // See if checked_out_svn already has any properties, which means it has already been initialized - in which case abort. 
-    if (isdir($svn_target) {
+    if (is_dir($svn_target)) {
         $properties = $rtrim($shell_exec("svn proplist $svn_target"));
         if ( $properies != "" ) {
             usage("Svn repo seems to have been already initialized.");
@@ -55,10 +53,10 @@
     }
     
     # @ pieter: 1) checkout HG under $checkout_directory . '_hg'
-    //Check if mecurial target exists, else clonse it from $from_hg
+    //Check if mecurial target exists, else clonse it from $from_hg_repo
     if ( ! is_dir($cloned_hg)) {
-        echo_verbose("Cloning Mercurial repository at {$from_hg} into Mercurial working copy at {$cloned_hg}.\n");
-        shell_exec("hg $quiet_flag clone $from_hg $cloned_hg");
+        cout("Cloning Mercurial repository at {$from_hg_repo} into Mercurial working copy at {$cloned_hg}.\n");
+        shell_exec("hg clone $from_hg_repo $cloned_hg");
     } else {
         usage("$cloned_hg already looks like it was inititalized. Perhaps rather use 'sync'.\n");
     }
@@ -69,109 +67,34 @@
     $start_rev = 0;
     
     # @ pieter: 2) create SVN repository under $checkout_directory . '_svn' (we should not need this in fact!! svnsync can work without. Please investigate)
-    //Check out svn repo
-    check_out_svn_repo($svn_repo,$svn_target);
- 
+    check_out_svn_repo($from_svn_repo,$svn_target);
     // Turn the SVN location into a mercurial one
-    echo_verbose("Converting Mercurial repository at {$cloned_hg} into Subversion working copy at {$svn_target}.\n");
+    cout("Converting Mercurial repository at {$cloned_hg} into Subversion working copy at {$svn_target}.\n");
     chdir($svn_target);
-    shell_exec("hg $quiet_flag init .");
+    shell_exec("hg init .");
     # @ pieter: 3) set level 0 SVN properties with the temporary directory name, hg repository, last fetched revision from hg ("" in this initial step)
     shell_exec("svn propset tempdir $checkout_directory .");
-    shell_exec("svn propset hgrepo $from_hg .");
+    shell_exec("svn propset hgrepo $from_hg_repo .");
     //Think 0 is a better value than ""
     shell_exec("svn propset last_fetched_rev 0 .");
-     
+    shell_exec("svn commit -m 'Initialized.'");
   }
-
-  function sync() {
-    $to_svn  = $_SERVER['argv'][2];
-
-    # @ pieter: contact SVN level 0 to fetch: temporary directory name, hg repository, last fetched revision
-    # @ pieter: see that the directory structure exists
-    # @ pieter: go synching from the last revision + 1, incrementally committing all changes, log entries, submitters, dates, ...
-    #   --> Watch out for file renames.
-  }
-/*
-      if ( $_SERVER['argv']['2'] ) {
-	  // **TODO** Confirm propr HG URL, extract HG name from URL for cleaner directory structure later.
-          $from_hg = $_SERVER['argv']['2'];
-          //Extract "clean" repo name for our folders.
-          $repo_name = get_clean_name($from_hg);
-
-	  $cloned_hg = $config['cache_dir'] . "$repo_name" . '_hg';
-          
-          $svn_target = $config['cache_dir'] . "$repo_name" . '_svn';
-
-          //Check if mecurial target exists, else clonse it from $from_hg
-          if ( ! is_dir($cloned_hg)) {
-              echo_verbose("Cloning Mercurial repository at {$from_hg} into Mercurial working copy at {$cloned_hg}.\n");
-              shell_exec("hg $quiet_flag clone $from_hg $cloned_hg");
-          } else { 
-              usage("$cloned_hg already looks like it was inititalized. Perhaps rather use 'sync'.\n");
-          }
-	  chdir($cloned_hg);
-
-	  $hg_tip_rev = rtrim(shell_exec('hg tip | head -1 | sed -e "s/[^ ]* *\([^:]*\)/\1/g"'));
-          if  (is_dir($svn_target)) {
-              usage("$svn_target already exists.  Perhaps rather use 'sync'.\n");
-          } else {
-              $svn_repo = $config['cache_dir'] . $repo_name . '_svn_repo';
-              $start_rev = 0;
-              create_svn_repo($svn_repo,$svn_target);
-              echo_verbose("Converting Mercurial repository at {$cloned_hg} into Subversion working copy at {$svn_target}.\n");
-              // Turn the SVN location into a mercurial one
-              chdir($svn_target);
-              shell_exec("hg $quiet_flag init .");
-          }
-      }  else {
-          usage("Please ensure mercurial src was specified after init.\n");
-      }
-  } elseif ( $_SERVER['argv']['1'] == 'sync' ) {
-      if ( $_SERVER['argv']['2'] ) {
-          $from_hg = $_SERVER['argv']['2'];
-          //Extract "clean" repo name for our folders.
-          $repo_name = get_clean_name($from_hg);
-	  $cloned_hg = $config['cache_dir'] . "$repo_name" . '_hg';
-          
-          $svn_target = $config['cache_dir'] . "$repo_name" . '_svn';
-	  
-          chdir($cloned_hg);
-          echo_verbose("Ensuring HG repo is up to date before sync.\n");
-          shell_exec("hg up");
-	  $hg_tip_rev = rtrim(shell_exec('hg tip | head -1 | sed -e "s/[^ ]* *\([^:]*\)/\1/g"'));
-
-          echo_verbose("Syncing Mercurial repository at {$from_hg} into Subversion working copy at {$svn_target}.\n");
-          $start_rev = $hg_tip_rev;
-      } else {
-          usage("Please ensure mercurial src was specified after sync.\n");
-      }
-  } else {
-      usage("usage: {$_SERVER['argv'][0]} init|sync mercurial_src \n");
-  }
-
-  $stop_rev = $hg_tip_rev;
-
-  //TODO maybe have the program do the svncheckout instead of the user having to it first.  Can do via subversion_repo config variable.
-
-  chdir($svn_target);
-  shell_exec("hg $quiet_flag pull -r $hg_tip_rev $from_hg");
-
+  
   for ($i = $start_rev; $i <= $stop_rev; $i++) {
-      echo_verbose("Fetching Mercurial revision $i/$hg_tip_rev\n");
-      rtrim(shell_exec("hg $quiet_flag update -C -r $i"));
+      cout("Fetching Mercurial revision $i/$hg_tip_rev\n");
+      rtrim(shell_exec("hg update -C -r $i"));
       //Parse out the incoming log message
       // **TODO**: see that we can fetch longer messages than 10 lines.
-      $hg_log_msg = rtrim(shell_exec("hg -R $from_hg -v log -r $i | grep -A10 ^description:$ | grep -v ^description:$ | head --lines=-2"));
-      $hg_log_changeset = rtrim(shell_exec("hg -R $from_hg -v log -r $i | grep ^changeset: | head -1"));
-      $hg_log_user = rtrim(shell_exec("hg -R $from_hg -v log -r $i | grep ^user: | head -1"));
-      $hg_log_date = rtrim(shell_exec("hg -R $from_hg -v log -r $i | grep ^date: | head -1"));
-      echo_verbose("- removing deleted files\n");
+      $hg_log_msg = rtrim(shell_exec("hg -R $from_hg_repo -v log -r $i | grep -A10 ^description:$ | grep -v ^description:$ | head --lines=-2"));
+      $hg_log_changeset = rtrim(shell_exec("hg -R $from_hg_repo -v log -r $i | grep ^changeset: | head -1"));
+      $hg_log_user = rtrim(shell_exec("hg -R $from_hg_repo -v log -r $i | grep ^user: | head -1"));
+      $hg_log_date = rtrim(shell_exec("hg -R $from_hg_repo -v log -r $i | grep ^date: | head -1"));
+      cout("- removing deleted files\n");
 
       shell_exec('svn status | grep \'^!\' | sed -e \'s/^! *\(.*\)/\1/g\' | while read fileToRemove; do svn remove "$fileToRemove"; done');
    
 
-      echo_verbose("- removing empty directories\n");
+      cout("- removing empty directories\n");
       // **TODO**: load into memory, creating files all around is the bash way ;-)
       $fp = fopen("/tmp/empty_dirs.txt", "w");
       fputs ($fp, shell_exec("find . -name '.svn' -prune -o -type d -printf '%p+++' -exec ls -am {} \; | grep '., .., .svn$' | sed -e 's/^\(.*\)+++.*/\1/g'"));
@@ -183,7 +106,7 @@
               echo $dir_to_remove;
               $dir_to_remove = rtrim($dir_to_remove); 
 	      shell_exec("rm -rf \"$dir_to_remove \"");
-              shell_exec("svn remove $quiet_flag \"$dir_to_remove\"");
+              shell_exec("svn remove \"$dir_to_remove\"");
           }  
           if (!feof($handle)) {
              echo "Error: unexpected fgets() fail\n";
@@ -191,7 +114,7 @@
           fclose($handle);
       }
 
-      echo_verbose("- adding files to SVN control\n");
+      cout("- adding files to SVN control\n");
       # 'svn add' recurses and snags .hg* files, we are pulling those out, so do our own recursion (slower but more stable)
       #   This is mostly important if you have sub-sites, as they each have a large .hg file in them
       $count = trim(shell_exec("svn status | grep '^\?' | grep -v '[ /].hg\(tags\|ignore\|sub\|substate\)\?\b' | wc -l"));
@@ -213,7 +136,7 @@
                       shell_exec("find \"$files_to_add\" -type d -name \".svn\" -exec rm -rf {} \\;");
                   }
 
-              shell_exec("svn add $quiet_flag --depth empty \"$files_to_add\"");
+              shell_exec("svn add --depth empty \"$files_to_add\"");
               }
               if (!feof($handle)) {
                   echo "Error: unexpected fgets() fail\n";
@@ -221,11 +144,9 @@
           fclose($handle);
           }
       } 
-      echo_verbose("- comitting\n");
+      cout("- comitting\n");
       /* might need consideration for symlinks, but not going to worry about that now. */
       $hg_log_msg="$hg_log_changeset\n$hg_log_user\n$hg_log_date\n$hg_log_msg";
-      $svn_commit_results = rtrim(shell_exec("svn commit $quiet_flag -m \"$hg_log_msg\""));
-      echo_verbose($svn_commit_results);
-  }
-*/
+      $svn_commit_results = rtrim(shell_exec("svn commit -m \"$hg_log_msg\""));
+      cout($svn_commit_results);
 }
