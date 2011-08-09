227294030ffac0e632e26d6b46b437d4545ec064
pieterste@gmail.com <pieterste@gmail.com@924969ee-89ad-3269-b9ab-9054e7bfb4fe>
2011-08-09 11:51:39 +0000

    Fix bug. More useful error messages if incorrectly used.

--- hg2svn.php
+++ hg2svn.php
@@ -26,7 +26,7 @@ if (VERBOSITY) {
 }
 
 if ( $_SERVER['argc'] < 2 ) {
-    usage();
+    usage("usage: {$_SERVER['argv'][0]} init|sync mercurial_src \n");
 }
 
 // **TODO**: What if I give 2 remote urls?
@@ -39,24 +39,24 @@ if ( $_SERVER['argv']['1'] == 'init' ) {
         //Extract "clean" repo name for our folders.
         $repo_name = get_clean_name($mercurial_src);
 
-	$mercurial_target = $config['cache_dir'] . '/' . "$repo_name" . '_hg';
+	$mercurial_target = $config['cache_dir'] . "$repo_name" . '_hg';
         
-        $subversion_target = $config['cache_dir'] . '/' . "$repo_name" . '_svn';
+        $subversion_target = $config['cache_dir'] . "$repo_name" . '_svn';
 
         //Check if mecurial target exists, else clonse it from $mercurial_src
         if ( ! is_dir($mercurial_target)) {
             echo_verbose("Cloning Mercurial repository at {$mercurial_src} into Mercurial working copy at {$mercurial_target}.\n");
             shell_exec("hg $quiet_flag clone $mercurial_src $mercurial_target");
         } else { 
-            usage(); //give warning that it already exists which is not the correct state for 'init'.
+            usage("$mercurial_target already looks like it was inititalized. Perhaps rather use 'sync'.\n");
         }
 	chdir($mercurial_target);
 
 	$hg_tip_rev = rtrim(shell_exec('hg tip | head -1 | sed -e "s/[^ ]* *\([^:]*\)/\1/g"'));
         if  (is_dir($subversion_target)) {
-            usage(); //give warning that it already exists which is not the correct state for 'init'.
+            usage("$subversion_target already exists.  Perhaps rather use 'sync'.\n");
         } else {
-            $svn_repo = $config['cache_dir'] . '/svn_repo';
+            $svn_repo = $config['cache_dir'] . $repo_name . '_svn_repo';
             $start_rev = 0;
             create_svn_repo($svn_repo,$subversion_target);
             echo_verbose("Converting Mercurial repository at {$mercurial_target} into Subversion working copy at {$subversion_target}.\n");
@@ -65,16 +65,16 @@ if ( $_SERVER['argv']['1'] == 'init' ) {
             shell_exec("hg $quiet_flag init .");
         }
     }  else {
-        usage();
+        usage("Please ensure mercurial src was specified after init.\n");
     }
 } elseif ( $_SERVER['argv']['1'] == 'sync' ) {
     if ( $_SERVER['argv']['2'] ) {
         $mercurial_src = $_SERVER['argv']['2'];
         //Extract "clean" repo name for our folders.
         $repo_name = get_clean_name($mercurial_src);
-	$mercurial_target = $config['cache_dir'] . '/' . "$repo_name" . '_hg';
+	$mercurial_target = $config['cache_dir'] . "$repo_name" . '_hg';
         
-        $subversion_target = $config['cache_dir'] . '/' . "$repo_name" . '_svn';
+        $subversion_target = $config['cache_dir'] . "$repo_name" . '_svn';
 	
         chdir($mercurial_target);
         echo_verbose("Ensuring HG repo is up to date before sync.\n");
@@ -83,8 +83,11 @@ if ( $_SERVER['argv']['1'] == 'init' ) {
 
         echo_verbose("Syncing Mercurial repository at {$mercurial_src} into Subversion working copy at {$subversion_target}.\n");
         $start_rev = $hg_tip_rev;
+    } else {
+        usage("Please ensure mercurial src was specified after sync.\n");
+    }
 } else {
-    usage();
+    usage("usage: {$_SERVER['argv'][0]} init|sync mercurial_src \n");
 }
 
 $stop_rev = $hg_tip_rev;
